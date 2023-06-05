63203500c0c5c684052c9c78c2ec65d32d2363a8
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-06-05 18:02:35 +0200

    user verified

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
 const dev = require('../config');
 
 const { securePassword } = require("../helpers/bcryptPassword");
@@ -70,6 +72,54 @@ const registerUser = async (req, res) => {
 
 const verifyEmail = (req, res) => {
     try {
+        const {token} = req.body;
+        if(!token) {
+            return res.status(404).json({
+                message: "token is missing"
+            })
+        }
+
+        jwt.verify(token, dev.jwtSecretKey, async function(err, decoded) {
+            if (err) {
+                return res.status(401).json({
+                    message: "Token is expired"
+            
+                });
+            }
+            // decoded the data
+            const { name, email, phone, hashedPassword, image  } = decoded;
+            const isExist = await User.findOne({email: email})
+        if(isExist){
+            return res.status(404).json({
+                message: 'user with email already exists'
+            })
+        }
+
+        // create the user
+
+        const newUser = new User({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            phone: phone,
+        })
+
+        if (image) {
+            newUser.image.data = fs.readFileSync(image.path);
+            newUser.image.contentType = image.type;
+        }
+        // save the user
+        const user = await newUser.save()
+        if(!user) {
+            res.status(400).json({
+                message: 'user was not repeated',
+            });
+        }
+        res.status(200).json({
+            message: 'user was created. Ready to sign in.'
+        })
+          });
+          
         res.status(200).json({
             message: "email is verified",
         })
