64b797e450d42accd05fbe487a097a295e2d2750
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-06-05 22:27:28 +0200

    routes added

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,18 @@
 const formidable = require('express-formidable')
 const router = require('express').Router();
 
-const { registerUser, verifyEmail } = require('../controllers/users');
+const { 
+    registerUser, 
+    verifyEmail,
+    loginUser,
+    logoutUser,
+    userProfile,
+ } = require('../controllers/users');
 
 router.post('/register', formidable(), registerUser)
 router.post('/verify-email', verifyEmail)
+router.post('/login', loginUser)
+router.get('/logout', logoutUser)
+router.post('/profile', userProfile)
 
 module.exports = router
