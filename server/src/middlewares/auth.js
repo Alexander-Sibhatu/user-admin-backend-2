730807ba25c5381209d234f2f180565445f41e67
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-06-09 00:19:58 +0200

    middleware added

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -9,5 +9,16 @@ const isLoggedIn = (req, res, next) => {
         console.log(error)
     }
 }
+const isLoggedOut = (req, res, next) => {
+    try {
+        if(req.session.userId) {
+            return res.status(400).json({ message: 'Please login!'});
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-module.exports = isLoggedIn
+module.exports = {isLoggedIn, isLoggedOut}
